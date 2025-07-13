c715ff219fe04eb431082c042a1538fa79979f8c
Hiten <hgeetaj@gmail.com>
2025-07-14 04:39:16 +0530

    fetch donors

--- routes/donationRoutes.js
+++ routes/donationRoutes.js
@@ -1,21 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Donation = require('../models/Donation');
+const Donation = require("../models/Donation");
 
-router.post('/', async(req, res) => {
-    try {
-        const donation = new Donation(req.body);
-        const saved = await donation.save();
+router.post("/", async (req, res) => {
+  try {
+    const donation = new Donation(req.body);
+    const saved = await donation.save();
 
-        res.status(201).json({
-            message: 'Donation saved successfully!',
-            data: saved,
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Error saving donation' });
-    }
+    res.status(201).json({
+      message: "Donation saved successfully!",
+      data: saved,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error saving donation" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const donors = await Donation.find().sort({ createdAt: -1 }).limit(50);
+    res.status(200).json(donors);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching donors" });
+  }
 });
 
 module.exports = router;
